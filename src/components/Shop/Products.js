03036054102ffbd5086455a30680614da9aced6b
amirsep <amir.sep19@gmail.com>
2022-11-24 18:39:44 +0530

    first commit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,40 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "The first book I ever Wrote",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second Book",
+    description: "The second book I ever Wrote",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => {
+          return (
+            <ProductItem
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
