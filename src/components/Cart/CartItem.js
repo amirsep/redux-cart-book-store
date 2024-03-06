c3b9236710d9f0639f7a0ebbd180f549d8484aa7
Mohammad Amir <107754398+amirsep@users.noreply.github.com>
2024-03-06 13:46:13 +0530

    Update CartItem.js

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -25,12 +25,12 @@ const CartItem = (props) => {
         <h3>{title}</h3>
         <div className={classes.price}>
            ₹ {total.toFixed(2)}{" "}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+          <span className={classes.itemprice}>(₹{price.toFixed(2)}/item)</span>
         </div>
       </header>
       <div className={classes.details}>
         <div className={classes.quantity}>
-          x <span>{quantity}</span>
+           <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
           <button onClick={removeItemHandler}>-</button>
