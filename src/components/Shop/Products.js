54252abc499471975f5fb6e02dcf42a10598888c
Mohammad Amir <107754398+amirsep@users.noreply.github.com>
2024-03-06 20:43:56 +0530

    Update Products.js

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -54,7 +54,9 @@ const Products = (props) => {
         })}
       </ul>
     </section>
-  <h5 className={classes.develop}> Made by ❤️ - Mohammad Amir | Frontend Developer </h5>
+        <div className={classes.develop}>
+  <h5 > Made by ❤️ - Mohammad Amir | Frontend Developer </h5>
+        </div >
 </>
   );
 };
