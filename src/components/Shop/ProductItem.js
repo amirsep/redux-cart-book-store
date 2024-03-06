af921c2776e7be0ef3c4231e845630e681896e77
Mohammad Amir <107754398+amirsep@users.noreply.github.com>
2024-03-06 13:54:13 +0530

    Update ProductItem.js

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -22,7 +22,7 @@ const ProductItem = (props) => {
       <Card>
         <header>
           <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
+          <div className={classes.price}>₹ {price.toFixed(2)}</div>
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
